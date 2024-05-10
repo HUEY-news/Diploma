2cc70c26c4a69424a009cfa9532f68ca6e300da7
yulia1Volkova <volkova.yulia88@yandex.ru>
2024-05-10 16:07:45 +0300

    Создан navigationGraph и фрагменты заглушки

---
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/RegionFragment.kt
@@ -0,0 +1,18 @@
+package ru.practicum.android.diploma.filter.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.practicum.android.diploma.R
+
+class RegionFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_region, container, false)
+    }
+}
