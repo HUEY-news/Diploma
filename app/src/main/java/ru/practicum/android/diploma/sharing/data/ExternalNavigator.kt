1eae6ed426cedd2eac5a447de4c146ce3117da94
Evlenium <Evlenium@mail.ru>
2024-05-11 14:40:14 +0300

    Init ExternalNavigator.kt, ResourceProvider.kt,Resource.kt, Debounce.kt

---
+++ app/src/main/java/ru/practicum/android/diploma/sharing/data/ExternalNavigator.kt
@@ -0,0 +1,6 @@
+package ru.practicum.android.diploma.sharing.data
+
+import android.content.Context
+
+class ExternalNavigator(val context: Context) {
+}
---
+++ app/src/main/java/ru/practicum/android/diploma/sharing/data/ResourceProvider.kt
@@ -0,0 +1,6 @@
+package ru.practicum.android.diploma.sharing.data
+
+import android.content.Context
+
+class ResourceProvider(val context: Context) {
+}
