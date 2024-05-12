b14f6816333781b17584b765d832767a0b3edfaf
Саид Исмаилов <HUEY.news@yandex.ru>
2024-05-12 10:11:35 +0300

    Diploma#36: Небольше правки в CodeStyle.

--- app/src/main/java/ru/practicum/android/diploma/search/data/db/AppDatabase.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/db/AppDatabase.kt
@@ -3,12 +3,7 @@ package ru.practicum.android.diploma.search.data.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(
-    version = 1,
-    entities = [
-        VacancyEntity::class
-    ]
-)
+@Database(version = 1, entities = [VacancyEntity::class])
 abstract class AppDatabase : RoomDatabase() {
     abstract fun vacancyDao(): VacancyDao
 }
