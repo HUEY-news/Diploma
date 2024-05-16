2cac6fc1fc2acceacda35f56ca5aa4c70e919e94
Саид Исмаилов <HUEY.news@yandex.ru>
2024-05-16 17:53:55 +0300

    Diploma#98: Указал корректный токен в SearchApiService.kt

--- app/src/main/java/ru/practicum/android/diploma/search/data/network/SearchApiService.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/network/SearchApiService.kt
@@ -4,17 +4,23 @@ import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Query
 import retrofit2.http.QueryMap
+import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.search.data.dto.SearchResponse
 
 interface SearchApiService {
     @Headers(
-        "Authorization: Bearer GH_HH_ACCESS_TOKEN",
-        "HH-User-Agent: Application Name (Работа для Федота)",
-        allowUnsafeNonAsciiValues = true,
+        "Authorization: Bearer $TOKEN",
+        "HH-User-Agent: Работа для Федота",
+        allowUnsafeNonAsciiValues = true
     )
     @GET("/vacancies?entity=vacancy")
     suspend fun searchVacancy(
         @Query("searchRequest") searchRequest: String,
         @QueryMap options: Map<String, String>,
     ): SearchResponse
+
+    companion object {
+        const val TOKEN = BuildConfig.HH_ACCESS_TOKEN
+    }
 }
+
