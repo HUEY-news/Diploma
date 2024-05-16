50fa890e92804f86e19b3024dff1a04e5bf801cf
yulia1Volkova <volkova.yulia88@yandex.ru>
2024-05-16 18:30:52 +0300

    собака, ну ты знаешь...

--- app/src/main/java/ru/practicum/android/diploma/convertor/DbConvertor.kt
+++ app/src/main/java/ru/practicum/android/diploma/convertor/DbConvertor.kt
@@ -6,13 +6,14 @@ import ru.practicum.android.diploma.search.domain.model.Vacancy
 class DbConvertor {
 
     fun map(vacancy: Vacancy): VacancyEntity = VacancyEntity(
-            vacancyId = vacancy.vacancyId,
-            vacancyTitle = vacancy.vacancyTitle,
-            vacancyDescription = vacancy.vacancyDescription)
+        vacancyId = vacancy.vacancyId,
+        vacancyTitle = vacancy.vacancyTitle,
+        vacancyDescription = vacancy.vacancyDescription
+    )
 
     fun map(vacancyEntity: VacancyEntity): Vacancy = Vacancy(
-            vacancyId = vacancyEntity.vacancyId,
-            vacancyTitle = vacancyEntity.vacancyTitle,
-            vacancyDescription = vacancyEntity.vacancyDescription
-        )
+        vacancyId = vacancyEntity.vacancyId,
+        vacancyTitle = vacancyEntity.vacancyTitle,
+        vacancyDescription = vacancyEntity.vacancyDescription
+    )
 }
