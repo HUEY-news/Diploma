5e79e007b3835c5ce740cdcd06f725024ed764c3
Саид <HUEY.news@yandex.ru>
2024-05-22 03:05:52 +0300

    Diploma#125: Скорректировал state-класс для возможности передачи флага вместе с контентом.

--- app/src/main/java/ru/practicum/android/diploma/details/presentation/model/StateLoadVacancy.kt
+++ app/src/main/java/ru/practicum/android/diploma/details/presentation/model/StateLoadVacancy.kt
@@ -3,12 +3,15 @@ package ru.practicum.android.diploma.details.presentation.model
 import ru.practicum.android.diploma.details.domain.model.Vacancy
 
 sealed interface StateLoadVacancy {
+
     object Loading : StateLoadVacancy
+
     data class Content(
-        val vacancies: Vacancy,
+        val data: Vacancy,
+        val isFavorite: Boolean
     ) : StateLoadVacancy
 
     data class Error(
-        val errorMessage: String,
+        val errorMessage: String
     ) : StateLoadVacancy
 }
