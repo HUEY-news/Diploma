bc7b8eb0eb62d7b97e04b6efd88a0392b68d3b9a
Саид <HUEY.news@yandex.ru>
2024-05-22 03:25:42 +0300

    Diploma#125: Реализовал взаимодействие ViewModel'и с data-слоем.

--- app/src/main/java/ru/practicum/android/diploma/details/presentation/VacancyDetailsViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/details/presentation/VacancyDetailsViewModel.kt
@@ -3,8 +3,10 @@ package ru.practicum.android.diploma.details.presentation
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import ru.practicum.android.diploma.details.domain.api.SearchDetailsInteractor
+import ru.practicum.android.diploma.details.domain.model.Vacancy
 import ru.practicum.android.diploma.details.presentation.model.StateLoadVacancy
 import ru.practicum.android.diploma.favorite.domain.api.FavoriteVacancyInteractor
 import ru.practicum.android.diploma.sharing.domain.api.ResourceInteractor
@@ -16,31 +18,37 @@ class VacancyDetailsViewModel(
     private val sharingInteractor: SharingInteractor,
     private val favoriteInteractor: FavoriteVacancyInteractor
 ) : ViewModel() {
+
+    private var currentVacancy: Vacancy? = null
+    private var isFavorite: Boolean = false
+
     private val vacancyLiveData = MutableLiveData<StateLoadVacancy>()
     fun observeVacancy(): MutableLiveData<StateLoadVacancy> = vacancyLiveData
+    private fun renderState(state: StateLoadVacancy) { vacancyLiveData.postValue(state) }
+
     fun searchRequest(id: String) {
-        vacancyLiveData.postValue(StateLoadVacancy.Loading)
+        renderState(StateLoadVacancy.Loading)
         viewModelScope.launch {
-            vacancyDetailsInteractor
-                .searchVacancy(id)
-                .collect { pair ->
-                    if (pair.first != null) {
-                        vacancyLiveData.postValue(
-                            StateLoadVacancy.Content(
-                                pair.first!!
-                            )
-                        )
-                    }
-                    when {
-                        pair.second != null -> {
-                            vacancyLiveData.postValue(
-                                StateLoadVacancy.Error(
-                                    errorMessage = resourceInteractor.getErrorInternetConnection()
-                                ),
-                            )
+
+            isFavorite = favoriteInteractor.isVacancyFavorite(id)
+
+            if (isFavorite) {
+                currentVacancy = favoriteInteractor.getVacancyFromFavoriteList(id)
+                renderState(StateLoadVacancy.Content(currentVacancy!!, isFavorite))
+            } else {
+                vacancyDetailsInteractor
+                    .searchVacancy(id)
+                    .collect { pair ->
+                        if (pair.first != null) {
+                            renderState(StateLoadVacancy.Content(pair.first!!, isFavorite))
+                        }
+                        when {
+                            pair.second != null -> {
+                                renderState(StateLoadVacancy.Error(resourceInteractor.getErrorInternetConnection()))
+                            }
                         }
                     }
-                }
+            }
         }
     }
 
@@ -54,5 +62,27 @@ class VacancyDetailsViewModel(
 
     fun shareVacancy(url: String) {
         sharingInteractor.shareVacancy(url)
+    }
+
+    fun onFavoriteClicked() {
+        currentVacancy?.let {
+            if (isFavorite) removeVacancy(currentVacancy!!)
+            else addVacancy(currentVacancy!!)
+        }
+    }
+
+    private fun removeVacancy(vacancy: Vacancy) {
+        viewModelScope.launch(Dispatchers.IO) {
+            favoriteInteractor.removeVacancyFromFavoriteList(vacancy)
+            renderState(StateLoadVacancy.Content(vacancy, false))
+            isFavorite = false
+        }
+    }
+
+    private fun addVacancy(vacancy: Vacancy) {
+        viewModelScope.launch(Dispatchers.IO) {
+            favoriteInteractor.addVacancyToFavoriteList(vacancy)
+            renderState(StateLoadVacancy.Content(vacancy, true))
+        }
     }
 }
