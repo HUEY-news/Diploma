30edd81445a51dddbf241d309f43c5b9552a9cd5
Саид <HUEY.news@yandex.ru>
2024-05-22 03:40:45 +0300

    Diploma#125: Удалил дубли моделей Salary и Employer.

--- app/src/main/java/ru/practicum/android/diploma/search/domain/model/Salary.kt
+++
@@ -1,9 +0,0 @@
-package ru.practicum.android.diploma.search.domain.model
-
-import java.math.BigDecimal
-
-data class Salary(
-    val currency: String?,
-    val from: BigDecimal?,
-    val to: BigDecimal?
-)
