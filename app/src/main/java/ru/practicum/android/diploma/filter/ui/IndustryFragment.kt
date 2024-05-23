96d0b13c549b8d00ffa1693f947f829484b004ef
yulia1Volkova <volkova.yulia88@yandex.ru>
2024-05-23 17:18:58 +0300

    Верстка Отрасли и добавление в граф навигации.

---
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/IndustryFragment.kt
@@ -0,0 +1,24 @@
+package ru.practicum.android.diploma.filter.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.practicum.android.diploma.R
+
+class IndustryFragment : Fragment() {
+
+
+ /*   override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }*/
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_industry, container, false)
+    }
+
+}
