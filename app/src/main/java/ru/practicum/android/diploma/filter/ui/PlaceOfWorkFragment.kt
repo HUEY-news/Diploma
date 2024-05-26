17e10797470a519744018f2da502aff6af5f0292
Evlenium <Evlenium@mail.ru>
2024-05-26 16:35:54 +0300

    Try to go CountryFragment

--- app/src/main/java/ru/practicum/android/diploma/filter/ui/PlaceOfWorkFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/PlaceOfWorkFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentFiltrationWorkplaceBinding
 
 class PlaceOfWorkFragment : Fragment() {
@@ -17,5 +20,33 @@ class PlaceOfWorkFragment : Fragment() {
     ): View {
         _binding = FragmentFiltrationWorkplaceBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            filtrationCountySelected.isClickable = true
+            filtrationCountySelected.setOnClickListener {
+                findNavController().navigate(R.id.action_placeOfWorkFragment_to_countryFragment)
+            }
+            filtrationRegionSelected.setOnClickListener {
+                findNavController().navigate(R.id.action_placeOfWorkFragment_to_regionFragment)
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+    companion object {
+        private const val ARGS_COUNTRY_NAME = "country_name"
+        private const val ARGS_REGION_NAME = "region_name"
+        fun createArgs(name: String): Bundle =
+            bundleOf(
+                ARGS_COUNTRY_NAME to name,
+                ARGS_REGION_NAME to name,
+            )
     }
 }
