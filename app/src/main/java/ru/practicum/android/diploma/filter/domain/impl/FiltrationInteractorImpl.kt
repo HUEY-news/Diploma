fa42d29f2b08282ca1a042aa224ab30b80713565
Саид <HUEY.news@yandex.ru>
2024-05-28 14:52:10 +0300

    Diploma#157: Реализовал интерфейс Interactor.

---
+++ app/src/main/java/ru/practicum/android/diploma/filter/domain/impl/FiltrationInteractorImpl.kt
@@ -0,0 +1,11 @@
+package ru.practicum.android.diploma.filter.domain.impl
+
+import ru.practicum.android.diploma.filter.domain.api.FiltrationInteractor
+import ru.practicum.android.diploma.filter.domain.api.FiltrationRepository
+import ru.practicum.android.diploma.filter.domain.model.Filter
+
+class FiltrationInteractorImpl(
+    private val repository: FiltrationRepository
+) : FiltrationInteractor {
+    override fun getFilter(): Filter? = repository.getFilter()
+}
