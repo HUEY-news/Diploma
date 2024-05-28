14de06e606f880d6b45d7a224d48ce0c4e4799e6
Саид <HUEY.news@yandex.ru>
2024-05-28 14:36:04 +0300

    Diploma#157: Описал модель фильтра.

--- app/src/main/java/ru/practicum/android/diploma/filter/domain/api/FiltrationRepository.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/domain/api/FiltrationRepository.kt
@@ -1,5 +1,7 @@
 package ru.practicum.android.diploma.filter.domain.api
 
+import ru.practicum.android.diploma.filter.domain.model.Filter
+
 interface FiltrationRepository {
     fun getFilter(): Filter?
 }
