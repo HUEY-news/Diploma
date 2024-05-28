bcf229e5d13cf49ff77f9df11bd2e7ce77166013
Саид <HUEY.news@yandex.ru>
2024-05-28 14:43:00 +0300

    Diploma#157: Реализовал интерфейс Repository, предоставляющий методы для записи и чтения настроек фильтрации.

---
+++ app/src/main/java/ru/practicum/android/diploma/filter/data/impl/FiltrationRepositoryImpl.kt
@@ -0,0 +1,31 @@
+package ru.practicum.android.diploma.filter.data.impl
+
+import com.google.gson.Gson
+import ru.practicum.android.diploma.filter.data.api.FiltrationStorage
+import ru.practicum.android.diploma.filter.domain.api.FiltrationRepository
+import ru.practicum.android.diploma.filter.domain.model.Filter
+
+class FiltrationRepositoryImpl(
+    private val storage: FiltrationStorage,
+    private val gson: Gson
+) : FiltrationRepository {
+
+    override fun getFilter(): Filter? {
+        val filter = storage.getFilter()
+
+        return if (filter.isEmpty() || filter =="null") { null }
+        else if (check(gson.fromJson(filter, Filter::class.java))) { null }
+        else gson.fromJson(filter, Filter::class.java)
+    }
+
+    private fun check(filter: Filter): Boolean =
+        with(filter) {
+            !isOnlyWithSalary &&
+                countryName == null &&
+                regionName == null &&
+                regionId == null &&
+                industryName == null &&
+                industryId == null &&
+                expectedSalary == null
+        }
+}
