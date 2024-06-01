43c205b59f68a6c9df0c648d82ad96d8dc38457e
Саид <HUEY.news@yandex.ru>
2024-06-01 14:48:34 +0300

    Diploma#206: Исправил отображение зарплаты на экране деталей вакансии.

--- app/src/main/java/ru/practicum/android/diploma/util/SalaryTools.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/SalaryTools.kt
@@ -1,32 +1,43 @@
 package ru.practicum.android.diploma.util
 
 import android.content.Context
+import ru.practicum.android.diploma.Currency
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.details.domain.model.Salary
+import java.text.NumberFormat
+import java.util.Locale
 
 fun getSalary(
     context: Context,
     salary: Salary?
 ): String {
+    val locale = Locale("ru")
+    val numberFormat = NumberFormat.getInstance(locale)
+
+    val currencySymbol = salary?.currency?.let { currencyCode ->
+        val currency = Currency.fromCode(currencyCode)
+        currency?.symbol ?: currencyCode
+    } ?: ""
+
     return when {
         salary == null -> context.getString(R.string.salary_not_specified)
         salary.from != null && salary.to != null -> context.getString(
             R.string.salary_from_to,
-            salary.from,
-            salary.to,
-            salary.currency
+            numberFormat.format(salary.from),
+            numberFormat.format(salary.to),
+            currencySymbol
         )
 
         salary.from != null -> context.getString(
             R.string.salary_from,
-            salary.from,
-            salary.currency
+            numberFormat.format(salary.from),
+            currencySymbol
         )
 
         salary.to != null -> context.getString(
             R.string.salary_to,
-            salary.to,
-            salary.currency
+            numberFormat.format(salary.to),
+            currencySymbol
         )
 
         else -> context.getString(R.string.salary_not_specified)
